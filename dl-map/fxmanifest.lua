decf009d2452ccc8ba2512946279502a042142bd
DevLua <79162417+DEVLUADL@users.noreply.github.com>
2021-04-05 01:08:00 -0400

    Add files via upload

---
+++ dl-map/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+description "FiveM OvalMap"
+author "DEVLUA"
+url "https://github.com/DEVLUADL"
+version "1.0"
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+files {
+	"hud.html"
+}
+
+ui_page "hud.html"
